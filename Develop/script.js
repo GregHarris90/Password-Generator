53efe30ad040bf430fab5533fbdefd73a41f42cf
Greg Harris <ghart.productions@gmail.com>
2021-01-12 23:33:50 -0800

    added criteria prompts for password

--- Develop/script.js
+++ Develop/script.js
@@ -2,7 +2,17 @@
 var generateBtn = document.querySelector("#generate");
 
 // todo: create prompts for password criteria
-// todo: prompt for length of password ( last 8 characters, no more than 128 characters)
+
+function mycriteria() {
+  var length = prompt("How long would you like your password to be?","Between 8 and 128 characters");
+  var lowercase = prompt ("Would you like lowercase letters?")
+  var uppercase = prompt ("Would you like uppercase letters?")
+  var numbers = prompt ("Would you like numbers?")
+  var symbols = prompt ("Would you like random symbols?")
+
+
+}
+// todo: prompt for length of password ( at least 8 characters, no more than 128 characters)
 // todo: prompt for character types to include such as: lowercase, uppercase, numeric, and/or special types
 // todo: make sure input for prompts is validated and (at least one of each type) is selected
 // todo: make sure selection of criteria is included in password
