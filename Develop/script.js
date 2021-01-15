cc8cb085c654ec939fabe3973f0967e386db01c6
Greg Harris <ghart.productions@gmail.com>
2021-01-14 23:50:52 -0800

    rebuilding main generator function, added notes to README

--- Develop/script.js
+++ Develop/script.js
@@ -12,20 +12,48 @@
 
 // DOM ELEMENTS:
 // Generate Button selector
-
 var generateBtn = document.querySelector("#generate");
 
 // Textarea for Password selector
-
 var passwordText = document.querySelector("#password");
 
 
-// Generates Password:
 
+var lower = "abcdefghijklmnopqrstuvwxyz"
+var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var numbers = "0123456789"
+var symbols = "!@#$%^&*()-_=+/?<>"
+
+var characterSet = [lower,upper,numbers,symbols]
+
+var password = ""
+
+var passwordLength = 8
+
+for(var i=0; i<8; i++){
+  var randomIndex = Math.floor(Math.random() * password.length)
+  password += characterSet[randomIndex]
+}
+
+var passwordEl = document.getElementById('password')
+passwordEl.textContent = password
+
+console.log(password);
+
+
+// Function to Generate Password:
 function generatePassword() {
 
+// 1) Write if/else to confirm!
+// 2) Write For loops!
+// 3) Write Index Array!
+
   var setlength = prompt("How long would you like your password to be?","Between 8 and 128 characters");
+  // if (setlength) {setlength = 
+  // }
   var confirmlowercase = confirm ("Would you like lowercase letters?");
+  // if (confirmlowercase) {confirmlowercase = true
+  // }
   var confirmuppercase = confirm ("Would you like uppercase letters?");
   var confirmnumbers = confirm ("Would you like numbers?");
   var confirmsymbols = confirm ("Would you like random symbols?");
@@ -36,64 +64,9 @@ console.log(confirmuppercase);
 console.log(confirmnumbers);
 console.log(confirmsymbols);
 
-// Write For loop!
 
-// Write Index Array!
 
-}
-
-// Attach eventListner to Generate Button:
-
-generateBtn.addEventListener("click", function() {
-
-  generatePassword();
-  var length = setlength.value;
-  var lower = confirmlowercase.value;
-  var upper = confirmuppercase.value;
-  var numbers = confirmnumbers.value;
-  var symbols = confirmsymbols.value;
-
-});
-
-// Variable for Values of Random Password:
-
-var ranPassword = {
-  lower : ranLower,
-  upper : ranUpper,
-  number : ranNumber,
-  symbol : ranSymbol,
-};
-
-// Random Functions to get lowercase, uppercase, numbers, symbols:
-
-  function ranLower(){
-    var lowercase = "abcdefghijklmnopqrstuvwxyz";
-    return lowercase[Math.floor(Math.random() * lowercase.length)];
-  }
-
-  function ranUpper(){
-    var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    return uppercase[Math.floor(Math.random() * uppercase.length)];
-  }
-
-  function ranNumber(){
-    var numbers = "0123456789";
-    return numbers[Math.floor(Math.random() * numbers.length)];
-  }
-
-  function ranSymbol(){
-    var symbols = "!@#$%^&*()-_=+/?<>";
-    return symbols[Math.floor(Math.random() * symbols.length)];
-
-  }
-
-  console.log(ranLower());
-  console.log(ranUpper());
-  console.log(ranNumber());
-  console.log(ranSymbol());
-
-  
-// Write password to the #password input
+// Write password to the #password input (PROVIDED)
 // function writePassword() {
 //   var password = generatePassword();
 //
@@ -101,5 +74,60 @@ var ranPassword = {
 
 // }
 
-// // Add event listener to generate button
+
+}
+
+// // Add event listener to generate button (PROVIDED)
 // generateBtn.addEventListener("click", writePassword());
+
+// Attach eventListner to Generate Button:
+generateBtn.addEventListener("click", function() {
+
+  //Run Generate Password Function:
+    generatePassword();
+  
+  });
+
+
+
+
+
+
+
+
+
+// ORIGINAL CODE: TA shared I was over complicating it!
+
+// // Variable for Values of Random Password:
+
+// var ranPassword = [ranLower, ranUpper, ranNumber, ranSymbol]
+
+// console.log(ranPassword);
+// // Random Functions to get lowercase, uppercase, numbers, symbols:
+
+//   function ranLower(){
+//     var lowercase = "abcdefghijklmnopqrstuvwxyz";
+//     return lowercase[Math.floor(Math.random() * lowercase.length)];
+//   }
+
+//   function ranUpper(){
+//     var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+//     return uppercase[Math.floor(Math.random() * uppercase.length)];
+//   }
+
+//   function ranNumber(){
+//     var numbers = "0123456789";
+//     return numbers[Math.floor(Math.random() * numbers.length)];
+//   }
+
+//   function ranSymbol(){
+//     var symbols = "!@#$%^&*()-_=+/?<>";
+//     return symbols[Math.floor(Math.random() * symbols.length)];
+
+//   }
+
+//   console.log(ranLower());
+//   console.log(ranUpper());
+//   console.log(ranNumber());
+//   console.log(ranSymbol());
+
--- README.md
+++ README.md
@@ -11,14 +11,15 @@ Homework assignment for Week 3 of Coding Bootcamp!
 
 # The Repository
 
-- This repository is for a web application to generate a randomized password using various parameters and 
+- This repository is for a web application to generate a randomized password using various parameters and setting certain criteria. 
 
 # User Story
 
-- A user will first certain parameters using the prompts provided. One will first set the length of the password (minimum 8 characters and maximum 128 characters), then confirm having lowercase letters, uppercase letters, numbers, and symbols added to their password. After clicking the generate button a randomized password should appear in the textbox above!
+- A user will select parameters using the prompts provided on the webpage. One will first set the length of the password (minimum 8 characters and maximum 128 characters), then confirm having lowercase letters, uppercase letters, numbers, and symbols added to their password. After clicking the generate button a randomized password should appear in the textbox above!
 
 # Password Generator
 
 
 # Overall Conclusion
 
+I will be turning in this assignment incomplete, as I really struggled with the javascript logic and writing all the syntax. Talked with a TA about how I got ahead of myself on this one and made my code more complicated than it needed to be. Because I ran out of time my README won't have pictures yet and the primary functions of this application will still need work! This started to make more sense after going over in in office hours, but it's still not finished. I will continue to work on this project until I get everything working. 
